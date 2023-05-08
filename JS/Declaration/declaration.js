da30a235096fd3cf86f1c434b74214c6715cb635
mishrabhi <asmyselfabhishek00@gmail.com>
2023-05-08 18:26:57 +0530

    added readme file

--- JS/Declaration/declaration.js
+++ JS/Declaration/declaration.js
@@ -10,8 +10,11 @@ console.log(x);  // 20
 
 console.log(y); //it throws an error as y is not defined (when variable is not declared)
 
-// coersion
+//variable hoisting:
 
+z = 5;  // Assigned 5 to z
+console.log(z);  
+var z;
 
 
 
