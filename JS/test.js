83523d3a26e2df31950d4b61537a43d34f57fb11
mishrabhi <asmyselfabhishek00@gmail.com>
2023-05-08 13:28:13 +0530

    learning JS

---
+++ JS/test.js
@@ -0,0 +1,14 @@
+console.log('Hello World!')
+
+// Variable declaration
+var x;
+console.log(x);  // undefined (when variable is declared but not assigned)
+
+x = 20;
+console.log(x);  // 20
+
+console.log(y); //it throws an error as y is not defined (when var is not declared)
+
+
+
+
