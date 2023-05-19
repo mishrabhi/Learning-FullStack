ccdce2fe737500b0c230312f14d31181161ccea0
mishrabhi <asmyselfabhishek00@gmail.com>
2023-05-19 16:13:59 +0530

    added if-else & switch conditions

---
+++ JS/Condition/if-else.js
@@ -0,0 +1,22 @@
+//if-else conditions
+
+var totalPurchase = 300;
+
+var discount = 20;
+
+if (totalPurchase <= 200) {
+    console.log("No discount")
+} else {
+    console.log("20% discount is applicable")
+}
+ //Output:  20% discount is applicable
+
+
+ var height = 170
+ if (height <= 170) {
+    console.log("You are rejected")
+ } else {
+    console.log("you are selected")
+ }
+// You are rejected
+
---
+++ JS/Condition/switch.js
@@ -0,0 +1,50 @@
+// Switch case:
+
+var programmingLang = 'JS';
+
+switch (programmingLang) {
+    case 'JS':
+        console.log('Learning JS')
+        break;
+
+        case 'Java':
+            console.log('Learning Java')
+            break;
+
+        case 'Python':
+            console.log('Learning python')
+            break;
+
+        default:
+            console.log('You are not a programmer')
+            break;
+
+}
+
+
+
+
+const pet = "dog";
+ 
+switch (pet) {
+  case "lizard":
+    console.log("I own a lizard");
+    break;
+  case "dog":
+    console.log("I own a dog");
+    break;
+  case "cat":
+    console.log("I own a cat");
+    break;
+  case "snake":
+    console.log("I own a snake");
+    break;
+  case "parrot":
+    console.log("I own a parrot");
+    break;
+  default:
+    console.log("I don't own a pet");
+    break;
+}
+
+
