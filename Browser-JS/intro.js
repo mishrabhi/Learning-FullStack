69fa102b4a68b075777f84dcd6006c0c9a14ff79
mishrabhi <asmyselfabhishek00@gmail.com>
2023-06-19 01:43:14 +0530

    added intro of browser side JS

---
+++ Browser-JS/intro.js
@@ -0,0 +1,17 @@
+// You know that Javascript program need a host environment. Most of what you learned so far was related to core ECMAScript/Javascript and can be used in many different host environments.
+
+//BOM and DOM Overview
+
+// The Javascript code in a page has access to a number of objects. These objects can be divided into the following types
+
+//Core ECMAScript objects: This consist of all the objects mentioned in the previous sessions.
+
+//DOM: This consist of objects that have to do with the currently loaded page, which is also called the document.
+
+//BOM: This consist of objects that deal with everything outside the page - the browser window and the desktop screen.
+
+
+
+//Window Object:
+
+//As you already know, in Javascript, there's global object provided by the host environment. In the browser environemnt, this global object is accessible using windows.
