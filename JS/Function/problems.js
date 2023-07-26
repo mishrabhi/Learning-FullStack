ae45cd21de5cc40813c869c67efdcbd03a3910c5
mishrabhi <asmyselfabhishek00@gmail.com>
2023-07-26 23:30:57 +0530

    added problems on function

--- JS/Function/problems.js
+++ JS/Function/problems.js
@@ -717,6 +717,19 @@ function stringToArray(string) {
 }
 
 
+//55. You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s) {
+    const wordLength = s.length;
+    const middleIndex = Math.floor(wordLength / 2);
+    if (wordLength % 2 == 0) {
+        return s.slice(middleIndex - 1, middleIndex + 1);
+    } else {
+        return s.charAt(middleIndex)
+    }
+}
+
+
 
 
 
