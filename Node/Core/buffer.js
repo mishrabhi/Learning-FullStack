8e83aa7acf26f7a146c1e0bfa6e705a6c39bdd78
mishrabhi <asmyselfabhishek00@gmail.com>
2023-08-06 18:45:49 +0530

    module and require in node

---
+++ Node/Core/buffer.js
@@ -0,0 +1,24 @@
+//Buffer:
+
+// The Buffer class was introduced as part of the Node.js API to make it possible to manipulate or interact with streams of binary data.
+// BUFFER is a temporary holding spot for data being moved from one place to another.
+
+// Stream
+
+// Stream in Node.js simply means a sequence of data being moved from one point to the other over time. The whole concept is, you have a huge amount of data to process, but you don’t need to wait for all the data to be available before you start processing it.
+
+// New Ways to create buffer
+
+
+// You can create using uninitiated Buffer of n octets like this.
+
+var buf = Buffer.alloc(10);
+
+console.log(Buffer.isBuffer(buf));     //true
+
+//You can also create a buffer using an array.
+//This initializes the buffer to the contents of this array. Keep in mind that the contents of the array are integers representing bytes.
+
+const buffer_array = Buffer.from([8, 6, 7, 5, 3, 0, 9]);   
+
+
---
+++ Node/Core/test-require.js
@@ -0,0 +1,12 @@
+// Requiring other modules 
+
+// In order to import the module, we need to use special keyword used to import things, and it is called 'require'.
+
+// Where module.exports lets us set things for export, require lets us specify modules to be imported into the current module.
+
+// require available in the global scope.
+
+// To import a defined in other file, we would require('relative path'), where we have specified the relative path.
+
+// module.exports is returned by require().
+
