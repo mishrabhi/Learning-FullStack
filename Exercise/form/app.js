8cdd8f39b2e0fc9c54df564f5f3ded9dd830e8e2
mishrabhi <asmyselfabhishek00@gmail.com>
2023-08-11 23:48:02 +0530

    form page exercise

---
+++ Exercise/form/app.js
@@ -0,0 +1,22 @@
+// function submitForm(){
+//     let firstName = document.querySelector("#firstName").value;
+//     let lastName = document.querySelector('#lastName').value;
+//     let gender = document.querySelector('#gender').value;
+//     let highestQualification = document.querySelector('#highest-qualification').value;
+//     let email = document.querySelector('#email').value;
+//     let password = document.querySelector('#password').value;
+
+// let formData = {
+//     firstName : firstName,
+//     lastName : lastName,
+//     gender : gender,
+//     highestQualification : highest-qualification,
+//     email : email,
+//     password : password
+
+// };
+
+// localStorage.setItem("formData", JSON.strigify(formData));
+
+// window.location.href = "display.html";
+// };
